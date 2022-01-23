8cf99b6bf0ef48418d103b2960c05dc073b0a7bf
Security-Development <35417717+Security-Development@users.noreply.github.com>
2022-01-23 17:05:18 +0900

    Update ExtendsLib.php

--- ExtendsLib.php
+++ ExtendsLib.php
@@ -11,6 +11,8 @@
  namespace Neo;
 
 use Closure;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\item\ItemFactory;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
@@ -29,5 +31,38 @@ class ExtendsLib extends PluginBase {
     public static function setItem(Player $player, int $slot, $id, string $name = "", int $count = 1) : void{
         $player->getInventory()->setItem($slot, ($item = ItemFactory::getInstance()->get($id)->setCount($count))->setCustomName(isset($name) ? $name : $item->getName()) );
     }
+
+    public static function initCommand(string $commandName, string $descript, Closure $function) {
+        Server::getInstance()->getCommandMap()->register($commandName, new class($commandName, $descript, $function) extends Command {
+
+            public function __construct(private string $commandName, private string $descript, private Closure $function) {
+                parent::__construct(
+                    $this->commandName,
+                    $this->descript
+                );
+            }
+
+
+            public function execute(CommandSender $sender, string $commandLabel, array $args) : bool {
+                ($this->function)($sender, $args);
+             return false;
+            }
+
+        });
+    }
+
+}
+
+class DataLib {
+    private static array $data = [];
+
+    public static function setData(array $value) : void {
+        self::$data = $value;
+    }
+
+    public static function getData() : array {
+        return self::$data;
+    }
+
 
 }
